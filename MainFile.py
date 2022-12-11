5b811f3d24620fe25dd58601e37bf1c72708fdf8
siddiqqulhakim <siddiqqulhakim@gmail.com>
2022-12-11 20:34:59 +0700

    tes2

---
+++ MainFile.py
@@ -0,0 +1,10 @@
+import httpimport
+
+# url = "https://gist.githubusercontent.com/siddiqqulhakim/675179664d17fb34b4df07804be5f88b/raw/485af02828f152c0775bd16c8756a0973a6e9444"
+# with httpimport.remote_repo(url):
+#     import hello
+# hello.abc()
+
+with httpimport.github_repo('siddiqqulhakim', 'test', branch='master'):
+    import test
+test.test()
--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-def test():
+def say():
     print('HELLO')
